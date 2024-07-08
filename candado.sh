74b677661c15949b5490bfa28899e52a652dbc7f
Santiluna99 <92774109+Santiluna99@users.noreply.github.com>
2024-07-07 23:37:09 -0300

    Create candado.sh

---
+++ candado.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "                      ...''...   .                                       
+                        ..;lolllllllloc,..                                      
+                      .,oxlcllllllllcccodd:.                                    
+                     .okc,colc;,'',:loo:,;ld;.                                  
+                    .xx,;dl.    .    .;x0x,;x:.                                 
+                   .oO',d;             .'dO,,d,                                 
+                 . ,0o.cl.   .           'Oo.cl.                                
+                 . ;Kc.lc                .xx.;l.   .  .      .  ...             
+                   :Kc.lc                .xx.;l.     ..;:::;..       ..         
+                   :Kc.lc.               .kx.;l. . .:oollllloo:. ..   .         
+                   :Kc.lc                .kx.;l.  'xd;:llclo:;dx' .   .         
+                   ;Kc.lc                .kx.;l. .xx'lk;...;ko'xd. ..           
+                ..;o0d:do::::::::::::::;;cOkcldc,,Oo.xd. . .dk.ok. .  ..        
+                :Odllllllllllollllllllllllllllllodxk;;xd:;:ok:;kl. . .          
+               .ok:;::;;,,'''''''''''''',,,;;;:;;occkl:clllc:lkl. .. ..         
+               .dk;;;,'...             ....',;:;;d: .coddlodoc'. ..             
+               .dO;;,.....         .  .......';;;d:   .dx,ol. ..                
+               .dk;,'.....   .;llcc'... .....';;;d:   .dx'ol.                   
+               .dO;,'....   .dkc,,cxc.    ...';;;d:   .xk'ol... .               
+               .dO;;,....   :0:    lk.    ...,;;;d:   .xk'll.  .                
+               .dO;,,'..    .ox;..cxc.  .....';,;d:   .xk'll.                   
+               .dO;,,'....   .xk.,kc     ....,;;;o;   .xx'll.                   
+               .dk;;;,...... .kd .xl  . .....,;;;d:   .xx.ll.                   
+               .dk;;;,...... ,0o..dd. .......,:;;o:   .xx'll....                
+               .dk;;;,'..... 'ol:co:.     ...';,;o;   .xx.;occoo'               
+               .dk:::,....  .     .   .    ...,,;d:   .xk.,l::od'               
+               .dk::c;,'....          ......',:;;d;   .xk.:xlcol.               
+               .dO;;:;;,'............  ......',';o;   .xx.,oolxk'               
+         .......;xdooooollllllllllllllllllclllllol'....ckdxo;::,.......         
+        ..;;;;,,.':::c:::::;;:::;;;;;;;::;;;;;;;,.......,;,..'''''',,'.         
+          ..........................................................      "
