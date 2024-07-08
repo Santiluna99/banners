46acf3a298be5c8a7568edbd21c88779551c1850
Santiluna99 <92774109+Santiluna99@users.noreply.github.com>
2024-07-07 23:34:53 -0300

    Create evil_moon.sh

---
+++ evil_moon.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "                                                                                
+                                ....'''''...                                    
+                         ..;coxkOOKXXNNNNXKK0kdl:,.                             
+                     ..,,;cccldkkkKKOXWWMWWWMWWWWWKko;.                         
+                  ..''..       .cc;oOXNWWWXo:coONMWWWWKxc.                      
+                .''.            .'.,lkkk0KXo.  .lXMWWWWWWXx;.                   
+              .''.             ....,cc,.''oXKxlcdXWWNWNKXWWNk;.                 
+             ...                   .'loc::okOO0KXNNN00NNXNOONNk,                
+           ...                       ':,,;odc;'',:x00OxO0XkcOWWXl.              
+          ''                         ...'.;,      .:okdkWWNKKNNNXk'             
+         ::                            .....       .,ckKXxlcookXNKk,            
+        :d.                             .          ..'dd'   ..'xXllk'           
+       'dc                                           .:..c:.   :0doXk.          
+      .c:.                                           .'.oXNo.  ,ldXWNl          
+      ,:.              .''..  ..                     .,lXWWX;  .,lKNWO.         
+     .;.              .oxdol;. ...                  ...cXMMWc  ..'xXNX:         
+     .,.              ;0XXXXx.   ...               .,. .OMMX;    .xxc0o         
+     .,               '0WWWW0'   ';''...    ..    ':oolkNMNo.   .'dc.xd         
+     .,                :KMWMWOlcxKKxl:,'',,,:;,'.':xXWWMNO:.   ;,.l:.kd         
+     .;.              ..,xXWMWWMWWWNKo.   ......   ':ll:'    'o:. :xkKo         
+     .:'        ....      .cdkO00kxl,                      ,lxo.  lXNK;         
+      ,:.        .......       .                        .:x0Oo'  'xxOx.         
+      .c;         ...',,,'....                      .;lxOKOOx'  .,'lO;          
+       'o;'.        .',;;;,;ccc:;,,,'........',;clokKXK000Oo.  ':';kl           
+        ,do:,.        .,:c;;llldxk00K00KKKKKXXNNNXOOKKKXXx'  .''.:xl.           
+         ,kO,..         .';cldddolxOkkOOxO000KKKKOx0NNKd,   .'..lkl.            
+          'xo.             ..,cdkxkKOk0kdkKXXNWWWKOkl:.    .'';xk:.             
+           .ld,      .         ..,;:oddkkxkOkxxoc,..      .;lx0x'               
+             ,do,..  ...        .      .....             'coxd;.                
+               ;oxOd;,'.        ....                  ..'okd:.                  
+                 'oxl'',,'                      ...   .col,.                    
+                   .,;'.';c;.                 ,:'''..,,,.                       
+                      ..''cOko;..            .,;,''''.                          
+                           .';c:;,''''''''',,,,'..                              "
